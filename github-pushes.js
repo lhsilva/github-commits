4808214c6af62cbb955dcf61bb03d40818fb50f5
lhsilva <lhsilva89@gmail.com>
2015-04-05 00:56:32 +0100

    Big Code Refactor and clean up

--- github-pushes.js
+++ github-pushes.js
@@ -1,4 +1,5 @@
-Polymer({
+/*global Polymer, XMLHttpRequest*/
+Polymer('github-pushes', {
     /**
      * "username" is the github username of the user we want to use.
      *
@@ -8,62 +9,111 @@ Polymer({
      */
     username: "",
 
-    nrOfPushes: 10,
+    /**
+     * "pushEventsNr" is the number of Push Events we want to retrieve.
+     *
+     * @property pushes
+     * @type number
+     * @default 10
+     */
+    pushEventsNr: 10,
 
-    isDebugEnabled: false,
 
-    ready: function () {
-        this.tableHeaders = {
-            pushDateTime: "Push Date and Time",
-            repository: "Repository",
-            commitMessage: "Commit Message"
-        };
-
-        this.events = [];
-        this.retrieveGithubLatestEvents(this.buildGithubPushesTable, this);
+    /**
+     * "tableHeaders" is the aggregator of the table headers values.
+     * 
+     * @property tableHeaders
+     * @type object
+     * @default "Push Date and Time, Repository, Commit Message"
+     */
+    tableHeaders: {
+        pushDateTime: "Push Date and Time",
+        repository: "Repository",
+        commitMessage: "Commit Message"
     },
 
-    //latest 90 days github public repos events
-    retrieveGithubLatestEvents: function (callback, scope) {
+    ready: function () {
+        'use strict';
+
+        this.pushEvents = [];
+        this.getUserPublicEvents(this.onGetUserPublicEventsResponse, this);
+    },
+
+    /**
+     * Constructs the API url and calls it. On ready state changes, calls the passed callback function.
+     * 
+     * @param {function} callback
+     * @param {object} scope
+     */
+    getUserPublicEvents: function (callback, scope) {
+        'use strict';
+
         var url = "https://api.github.com/users/" + this.username + "/events/public",
             httpRequest;
 
-        try {
-            httpRequest = new XMLHttpRequest();
-            httpRequest.onreadystatechange = function () {
-                if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-                    var events = JSON.parse(httpRequest.responseText);
-                    callback.call(scope, events);
-                }
-            };
-            httpRequest.open("GET", url, true);
-            httpRequest.send(null);
-        } catch (ex) {
-            if (this.isDebugEnabled) {
-                console.log("Error while creating a request to " + url);
+        httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+                callback.call(scope, httpRequest.responseText);
             }
+        };
+
+        httpRequest.open("GET", url, true);
+        httpRequest.send(null);
+    },
+
+    /**
+     * Takes care of the getUserPublicEvents response.
+     * 
+     * @param {object} response
+     */
+    onGetUserPublicEventsResponse: function (response) {
+        'use strict';
+
+        var events = JSON.parse(response.responseText);
+        if (events) {
+            this.pushEvents = this.extractPushEvents(events);
         }
     },
 
-    buildGithubPushesTable: function (events) {
-        var counter = 1;
+    /**
+     * Extracts and instantiates an array of Push Events from the passed events array.
+     * 
+     * @param {Array} events
+     * @returns {Array} pushEvents
+     */
+    extractPushEvents: function (events) {
+        'use strict';
 
-        for (var i = 0, n = events.length; i < n && counter <= this.nrOfPushes; i++) {
-            var event = events[i];
+        var counter = 1,
+            pushEvents = [],
+            event, pushEvent;
 
-            // Other events include Creation, Watch and so on.
-            if (event && event.type === "PushEvent" && event.actor.login === this.username) {
-                this.createTableRow(event, counter);
+        for (var i = 0, n = events.length; i < n && counter <= this.pushEventsNr; i++) {
+            event = events[i];
+
+            if (event && event.type === "PushEvent") {
+                pushEvent = this.createPushEvent(event, counter);
+                pushEvents.push(pushEvent);
                 counter++;
             }
         }
+
+        return pushEvents;
     },
 
-    createTableRow: function (rawEvent, counter) {
-        var creationDateComplete = new Date(rawEvent.created_at),
-            repository = rawEvent.repo,
-            commit = rawEvent.payload.commits[0],
-            event = {
+    /**
+     * Creates and returns a small Push Event instance with the needed information from the raw Push Event passed.
+     * 
+     * @param {object} pushEvent
+     * @param {number} counter
+     * @returns {object} litePushEvent
+     */
+    createPushEvent: function (pushEvent, counter) {
+        var creationDateComplete = new Date(pushEvent.created_at),
+            repository = pushEvent.repo,
+            commit = pushEvent.payload.commits[0],
+            litePushEvent = {
                 id: counter,
                 creationDate: creationDateComplete.toLocaleDateString(),
                 creationTime: creationDateComplete.toLocaleTimeString(),
@@ -72,6 +122,6 @@ Polymer({
                 commitMessage: commit.message
             };
 
-        this.events.push(event);
+        return litePushEvent;
     }
 });
